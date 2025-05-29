0a9c7df0ba69f8338c94c313cd49fad58b5ea9cd
jon998 <yongc998@gmail.com>
2025-05-29 08:02:48 -0600

    first commit

---
+++ android/app/src/main/java/com/exampleCapgo/updater/MainActivity.java
@@ -0,0 +1,5 @@
+package com.exampleCapgo.updater;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.exampleCapgo.updater',
+  appName: 'updatercapgoexample',
+  webDir: 'www'
+};
+
+export default config;
